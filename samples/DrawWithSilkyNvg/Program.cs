b87b8f71af545034a0222da8839c1d47d49d5f2d
Dylan Perks <dylanperks23@gmail.com>
2024-03-07 21:25:40 -0600

    Add SilkyNvg experiment

---
+++ samples/DrawWithSilkyNvg/Program.cs
@@ -0,0 +1,115 @@
+﻿// Copyright (c) Six Labors.
+// Licensed under the Six Labors Split License.
+
+// ReSharper disable AccessToDisposedClosure
+using System.Collections.Generic;
+using DrawWithSilkyNvg;
+using ImGuiNET;
+using Silk.NET.Input;
+using Silk.NET.Maths;
+using Silk.NET.OpenGL;
+using Silk.NET.OpenGL.Extensions.ImGui;
+using Silk.NET.Windowing;
+using SilkyNvg;
+using SilkyNvg.Rendering.OpenGL;
+using SixLabors.Fonts;
+
+OpenGLRenderer renderer = null!;
+
+SilkyNvgGlyphRenderer glypher = null!;
+
+ImGuiController imgui = null!;
+
+IInputContext input = null!;
+
+GL gl = null!;
+
+const string title = "Vector Text Demo using Silk.NET, SilkyNvg, and SixLabors.Fonts!";
+
+// Create a window for OpenGL 4.5.
+IWindow window = Window.Create(WindowOptions.Default with
+{
+    API = new GraphicsAPI(ContextAPI.OpenGL, ContextProfile.Core, ContextFlags.Default, new APIVersion(4, 5)),
+    Title = title,
+    PreferredBitDepth = Vector4D<int>.One * 8,
+    PreferredDepthBufferBits = 24,
+    PreferredStencilBufferBits = 8
+});
+
+// Set everything up when the window loads up
+window.Load += () =>
+{
+    // Create the OpenGL API object for our window's context.
+    gl = window.CreateOpenGL();
+
+    // Create the input context for receiving keyboard and mouse input.
+    input = window.CreateInput();
+
+    // Create the ImGui controller
+    imgui = new ImGuiController(gl, window, input);
+
+    // Create the SilkyNvg renderer
+    renderer = new OpenGLRenderer(CreateFlags.Antialias | CreateFlags.Debug, gl);
+    glypher = new SilkyNvgGlyphRenderer { Nvg = Nvg.Create(renderer) };
+    gl.Viewport(window.FramebufferSize);
+};
+
+var fonts = new List<Font>();
+
+var demoWindow = false;
+
+window.Update += deltaSecs =>
+{
+    imgui.Update((float)deltaSecs);
+    if (ImGui.Begin("Interactive Demo"))
+    {
+        var checkVal = glypher.DrawGlyphBox;
+        ImGui.Checkbox("Show Glyph Bounding Boxes", ref checkVal);
+        glypher.DrawGlyphBox = checkVal;
+        checkVal = glypher.DrawTextBox;
+        ImGui.Checkbox("Show Text Bounding Boxes", ref checkVal);
+        glypher.DrawTextBox = checkVal;
+        ImGui.Checkbox("Show ImGui Demo Window", ref demoWindow);
+        if (demoWindow)
+        {
+            ImGui.ShowDemoWindow();
+        }
+
+        // TODO add/edit/remove fonts in use by demo
+        // TODO add/edit/remove test text
+        ImGui.End();
+    }
+};
+
+window.Render += deltaSecs =>
+{
+    window.Title = $"{title} | {1 / deltaSecs} FPS";
+
+    gl.ClearColor(Vector4D<float>.Zero);
+    gl.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit | ClearBufferMask.StencilBufferBit);
+    glypher.Nvg.BeginFrame(window.FramebufferSize.As<float>(), 1);
+
+    Font font = SystemFonts.CreateFont("arial", 40f);
+    var tr = new TextRenderer(glypher);
+    tr.RenderText("The quick brown fox jumps over the lazy dog.", new TextOptions(font));
+
+    glypher.Nvg.EndFrame();
+    gl.Viewport(window.FramebufferSize);
+    imgui.Render();
+};
+
+window.Closing += () =>
+{
+    renderer.Dispose();
+    gl.Dispose();
+    input.Dispose();
+};
+
+window.FramebufferResize += newSize =>
+{
+    gl.Viewport(newSize);
+};
+
+window.Run();
+
+window.Dispose();
---
+++ samples/DrawWithSilkyNvg/SilkyNvgGlyphRenderer.cs
@@ -0,0 +1,96 @@
+﻿// Copyright (c) Six Labors.
+// Licensed under the Six Labors Split License.
+
+using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
+using Silk.NET.Maths;
+using SilkyNvg;
+using SilkyNvg.Graphics;
+using SilkyNvg.Paths;
+using SixLabors.Fonts;
+
+namespace DrawWithSilkyNvg;
+
+[SuppressMessage("ReSharper", "ArrangeModifiersOrder", Justification = "StyleCop and ReSharper fight eachother")]
+public class SilkyNvgGlyphRenderer : IColorGlyphRenderer
+{
+    required public Nvg Nvg { get; init; }
+
+    public bool DrawTextBox { get; set; }
+
+    public bool DrawGlyphBox { get; set; }
+
+    /// <inheritdoc />
+    public void BeginFigure()
+    {
+        // Span<byte> col = stackalloc byte[3];
+        // Random.Shared.NextBytes(col);
+        this.Nvg.BeginPath();
+        //this.Nvg.FillColour(new Colour(col[0], col[1], col[2], 255));
+    }
+
+    // DEVELOPER NOTE: I could've sworn we added an implicit cast in Silk.NET between System.Numerics vectors and
+    // Silk.NET.Maths vectors, perhaps look at adding this in Silk.NET. Also SilkyNvg disregarded the advice of "use the
+    // speedy System.Numerics APIs for floats, and only use Silk.NET.Maths for everything else"
+
+    /// <inheritdoc />
+    public void MoveTo(Vector2 point)
+    {
+        // Span<byte> col = stackalloc byte[3];
+        // Random.Shared.NextBytes(col);
+        // this.Nvg.FillColour(new Colour(col[0], col[1], col[2], 255));
+        this.Nvg.MoveTo(new Vector2D<float>(point.X, point.Y));
+    }
+
+    /// <inheritdoc />
+    public void QuadraticBezierTo(Vector2 secondControlPoint, Vector2 point)
+        => this.Nvg.QuadTo(
+            new Vector2D<float>(secondControlPoint.X, secondControlPoint.Y),
+            new Vector2D<float>(point.X, point.Y));
+
+    public void CubicBezierTo(Vector2 secondControlPoint, Vector2 thirdControlPoint, Vector2 point)
+        => this.Nvg.BezierTo(
+            new Vector2D<float>(secondControlPoint.X, secondControlPoint.Y),
+            new Vector2D<float>(thirdControlPoint.X, thirdControlPoint.Y),
+            new Vector2D<float>(point.X, point.Y));
+
+    /// <inheritdoc />
+    public void LineTo(Vector2 point) => this.Nvg.LineTo(new Vector2D<float>(point.X, point.Y));
+
+    /// <inheritdoc />
+    public void EndFigure() => this.Nvg.Fill();
+
+    /// <inheritdoc />
+    public void EndGlyph() => this.Nvg.Restore();
+
+    /// <inheritdoc />
+    public bool BeginGlyph(in FontRectangle bounds, in GlyphRendererParameters parameters)
+    {
+        this.Nvg.Save();
+        return true;
+    }
+
+    /// <inheritdoc />
+    public void EndText()
+    {
+        // Everything Is Ticketty-Boo! Which is code for "this function does sweet F.A. in this impl".
+    }
+
+    /// <inheritdoc />
+    public void BeginText(in FontRectangle bounds)
+    {
+        // Everything Is Ticketty-Boo! Which is code for "this function does sweet F.A. in this impl".
+    }
+
+    /// <inheritdoc />
+    public TextDecorations EnabledDecorations() => TextDecorations.None;
+
+    /// <inheritdoc />
+    public void SetDecoration(TextDecorations textDecorations, Vector2 start, Vector2 end, float thickness)
+        => Debug.WriteLine($"attempted to decorate text: {textDecorations} / {start} / {end} / {thickness}");
+
+    /// <inheritdoc />
+    public void SetColor(GlyphColor color) => this.Nvg.FillColour(new Colour(color.Red, color.Green, color.Blue, color.Alpha));
+}
